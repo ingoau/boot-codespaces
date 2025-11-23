fc0c12873d655c8f1a22d2bce2843943d3800698
Ingo Wolf <me@ingowolf.au>
2025-11-23 16:39:17 +1100

    add action to create codespace

---
+++ lib/actions.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { Octokit } from "octokit";
+import { auth } from "./auth";
+import { headers } from "next/headers";
+
+async function createCodespace() {
+  const headersList = await headers();
+
+  const { accessToken } = await auth.api.getAccessToken({
+    headers: headersList,
+    body: {
+      providerId: "github",
+    },
+  });
+
+  const octokit = new Octokit({
+    auth: accessToken,
+  });
+
+  const response = await octokit.request(
+    "POST /repos/{owner}/{repo}/codespaces",
+    {
+      owner: "Inglan",
+      repo: "boot-codespaces",
+      ref: "main",
+      machine: "standardLinux32gb",
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
+
+  return response.data.name;
+}
