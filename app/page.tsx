95bbe300cc03ded9a7f6ce48c37ed3ce1c70f95d
Ingo Wolf <me@ingowolf.au>
2025-11-23 16:29:35 +1100

    implement user session display on home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,15 @@
 import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
+  const session = authClient.useSession();
+  if (session.data?.user) {
+    return <div>Signed in as {session.data.user.name}</div>;
+  }
   return (
-    <>
+    <div>
       <button onClick={() => authClient.signIn.social({ provider: "github" })}>
         Sign in with Github
       </button>
-    </>
+    </div>
   );
 }
